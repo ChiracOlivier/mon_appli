c76c5eb081eb225b62cfd1b80fa5af6b04316fa4
Chiracolivier <chiracolivier28@gmail.com>
2019-08-15 17:02:10 +0200

    Initial commit

---
+++ src/test/java/org/example/demo/AppTest.java
@@ -0,0 +1,32 @@
+package org.example.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+     @Test
+    public  void shouldReturn_the_Sum(){
+        //arrange
+        Integer expected=3;
+        Calculator calculator= new Calculator();
+        //act
+        Integer result= calculator.add(1,2);
+         //assert
+          assertEquals(expected, result);
+     }
+}
